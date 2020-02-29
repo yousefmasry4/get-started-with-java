55a7850944cbc8371fe3220135417b83eb7a413b
youssef <yousefmasry4>
2020-02-29 02:17:17 +0200

    loop

---
+++ src/com/sudo/loops_1.java
@@ -0,0 +1,20 @@
+package com.sudo;
+
+import java.util.Scanner;
+
+public class loops_1 {
+    public static void main(){
+        Scanner scanner=new Scanner(System.in);
+
+        int x=scanner.nextInt();
+
+        scanner.close();
+
+
+        for(int i=1;i<=10;i++){
+            System.out.printf("%d x %d = %d\n",x,i,x*i);
+        }
+
+
+    }
+}

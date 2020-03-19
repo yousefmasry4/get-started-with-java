f82b1ffb6850ad45ad478bf78f61cefc4884e434
youssef <youssef#sudoai.codes>
2020-03-19 04:39:40 +0200

    oop

---
+++ src/com/sudo/Interface.java
@@ -0,0 +1,47 @@
+package com.sudo;
+import java.util.Scanner;
+
+
+interface AdvancedArithmetic{
+    int divisor_sum(int n);
+}
+
+
+class MyCalculator implements  AdvancedArithmetic{
+
+    @Override
+    public int divisor_sum(int n) {
+        int result=n;
+        for(int i=1;i<n/2+1;i++){
+
+            if(0 == n%i){
+                result +=i;
+            }
+        }
+        return result;
+    }
+}
+
+
+
+public class Interface {
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+        sc.close();
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
